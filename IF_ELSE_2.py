e55d5e11f8261fa5724dd5761ad2066c7b6311a7
Vivasvat-pro <maddimsettiviv@gmail.com>
2026-07-08 20:46:38 +0530

    Add files via upload

---
+++ IF_ELSE_2.py
@@ -0,0 +1,7 @@
+actual_cost = float(input("Enter the actual cost of the item: "))
+sale_amount = float(input("Enter the sale amount: "))
+if (sale_amount > actual_cost):
+    amount = sale_amount - actual_cost
+    print("total profit = {0}".format(amount))
+else:
+    print("No profit $$$")

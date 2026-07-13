b23a2d1e5334510a15688285873f11779711fdcd
Vivasvat-pro <maddimsettiviv@gmail.com>
2026-07-13 20:50:41 +0530

    Add files via upload

---
+++ and_or.py
@@ -0,0 +1,21 @@
+a = 10
+b = 90000000
+c = 2
+if a and b and c:
+    print("if a and b and c are true ")
+else:
+    print("else a nd b and c are false")
+
+a = 20
+b = -20
+c = 0
+if a>0 or b>0:
+    print("either of the number is greater than zero")
+else:
+    print("no number is greater than 0")
+
+
+if b>0 or c>0:
+    print("either of the number is greater than zero")
+else:
+    print("no number is greater than 0")

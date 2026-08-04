977c5832e3e1c192c2cb9aefa071dd992dc3fd38
Vivasvat-pro <maddimsettiviv@gmail.com>
2026-08-04 20:20:35 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,4 @@
+radius = int(input("enter the radius of the circle:"))
+def area(radius):
+    return 22/7 * radius * radius
+print("The area of the circle is:", area(radius))
---
+++ circumfrence.py
@@ -0,0 +1,4 @@
+radius = int(input("enter the radius of the circle:"))
+def circumfrence(radius):
+    return 22/7 * radius * 2
+print("The circumference of the circle is:", circumfrence(radius))
